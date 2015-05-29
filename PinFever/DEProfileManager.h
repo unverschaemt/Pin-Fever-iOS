9d719b98bb6d108700755f5140cd962331f0389f
David Ehlen <dehlen@me.com>
2015-05-29 11:03:21 +0200

    Added other wrapper classes to retrieve the current player with singleton and to compress avatar image for upload

---
+++ PinFever/DEProfileManager.h
@@ -0,0 +1,13 @@
+//
+//  DEProfileManager.h
+//  PinFever
+//
+//  Created by David Ehlen on 29.05.15.
+//  Copyright (c) 2015 David Ehlen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DEProfileManager : NSObject
+
+@end
---
+++ PinFever/DEProfileManager.m
@@ -0,0 +1,13 @@
+//
+//  DEProfileManager.m
+//  PinFever
+//
+//  Created by David Ehlen on 29.05.15.
+//  Copyright (c) 2015 David Ehlen. All rights reserved.
+//
+
+#import "DEProfileManager.h"
+
+@implementation DEProfileManager
+
+@end
