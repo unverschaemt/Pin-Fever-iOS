a3d698b2341735db9a74ad46775b5b7a3267f1b8
David Ehlen <dehlen@me.com>
2015-06-01 01:15:28 +0200

    added API Routes

--- PinFever/Constants.h
+++ PinFever/Constants.h
@@ -19,19 +19,29 @@
 #define kAPISetPlayerEndpoint (kAPIEndpointHost @"/players/me/set")
 #define kAPIUploadAvatarEndpoint (kAPIEndpointHost @"/players/me/avatarupload")
 #define kAPIDownloadAvatarEndpoint (kAPIEndpointHost @"/players/me/img.jpeg")
-
-
+#define kAPIFindAutoGameEndpoint (kAPIEndpointHost @"/turnbasedmatch/findauto")
+#define kAPICreateGameEndpoint (kAPIEndpointHost @"/turnbasedmatch/create")
 #define kErrorKey @"err"
 #define kDataKey @"data"
 #define kTokenKey @"token"
 #define kFriendKey @"friends"
 #define kPlayersKey @"players"
 #define kPlayerKey @"player"
+#define kAutoGame @"autoGame"
 
 #define kIdKey @"_id"
 #define kDisplayName @"displayName"
 #define kEmailKey @"email"
 #define kLevelKey @"level"
+#define kStateKey @"state"
+#define kModeKey @"mode"
+#define kMatchIdKey @"matchId"
+#define kParticipantsKey @"participants"
+#define kNumberOfPlayersKey @"numberOfPlayers"
+#define kMinLevelKey @"numberOfPlayers"
+#define kCreatedKey @"created"
+#define kTurnsKey @"turns"
+#define kTurnBasedMatchKey @"TurnBasedMatch"
 
 #define kKeychainKey @"PinFeverKeychain"
 
